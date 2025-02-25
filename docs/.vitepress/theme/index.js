6299d6c01e37babdc0c5b9ce0fcc2293fe0b7720
xkloveme <xkloveme@gmail.com>
2025-02-25 16:16:08 +0800

    docs: 更新文档站点配置和依赖
    - 在主题中集成 Google Analytics
    - 升级 vitepress 到 1.6.3
    - 更新 vitepress-plugin-tabs 到 0.6.0
    - 添加 vitepress-plugin-google-analytics 依赖

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,13 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
-
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    googleAnalytics({
+      id: 'G-73ZHQ52XPJ',
+    })
     enhanceAppWithTabs(app)
   }
 }
